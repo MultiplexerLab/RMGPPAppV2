69d79ff7458283025d2ee220d37f8d0d56a7f2d5
rowshni-git <50178678+rowshni-git@users.noreply.github.com>
2020-01-14 10:06:00 +0600

    Update Endpoints.java

--- app/src/main/java/ipa/rmgppapp/helper/Endpoints.java
+++ app/src/main/java/ipa/rmgppapp/helper/Endpoints.java
@@ -1,32 +1,34 @@
 package ipa.rmgppapp.helper;
 
 public class Endpoints {
-    public static final String BASE_URL = "https://rmgppapp.com/android_api/";
-    public static final String BASE_URL2 = "https://rmgpp.rmgppapp.com/api/";
-    public static final String CHECK_SUPERVISOR_URL = BASE_URL + "checkSupervisor.php";
-    public static final String POST_ASSIGNED_WORKER_URL = BASE_URL + "insertAssignedWorker.php";
-    public static final String GET_HR_DATA_URL = BASE_URL + "hrData.php";
-    public static final String GET_PLANNING_DATA_URL = BASE_URL + "planningData.php";
-    public static final String GET_OPERATION_DATA_URL = BASE_URL + "operationData.php";
-    public static final String POST_HOURLY_DATA_URL = BASE_URL + "insertHourlyRecord.php";
-    public static final String POST_LINE_DATA_URL = BASE_URL + "insertLineData.php";
-    public static final String POST_LINE_TARGET_URL = BASE_URL + "insertLineTarget.php";
-    public static final String GET_HOURLY_RECORD_DATA = BASE_URL + "getHourlyData.php";
-    public static final String GET_ASSIGNED_WORKER_URL = BASE_URL + "getAssignedWorkerData.php";
-    public static final String GET_SUMMERY_DATA = BASE_URL + "getSummeryData.php";
-    public static final String POST_NEW_STYLE_URL = BASE_URL + "insertStyle.php";
-    public static final String CHECK_LINE_TARGET_URL = BASE_URL + "checkLineTarget.php";
-    public static final String GET_PROBLEM_DATA_URL = BASE_URL2 + "getProblems/";
-    public static final String GET_ALL_STYLES = BASE_URL + "getAllStyles.php";
-    public static final String GET_ALL_STYLES_OB = BASE_URL + "getAllStylesOB.php";
-    public static final String GET_STYLE_DETAILS = BASE_URL + "getStyleDetails.php";
-    public static final String GET_STYLE_DETAILS_FROM_ORDERLIST = BASE_URL + "getStyleDetailsFromOrderList.php";
-    public static final String DELETE_STYLE_URL = BASE_URL + "deleteStyle.php";
-    public static final String DELETE_LINE_DATA_URL = BASE_URL + "deleteLinedata.php";
-    public static final String GET_LINE_RECORD = BASE_URL + "getLineData.php";
-    public static final String UPDATE_LINE_DATA_STATUS = BASE_URL + "updateStatus.php";
-    public static final String UPDATE_STYLE_STATUS = BASE_URL + "updateStatusStyle.php";
-    public static final String GET_STYLE_SUMMERY = BASE_URL2+ "getLineSummary/";
-    public static final String GET_BUILDING_DATA = BASE_URL2 + "getBuildingData/";
-    public static final String CHECK_DEVICEID = "https://beta.rmgppapp.com/api/checkValidFactory/";
+    // public static final String BASE_URL = "https://rmgppapp.com/android_api/";
+    // public static final String BASE_URL2 = "https://rmgpp.rmgppapp.com/api/";
+
+    public static final String BASE_URL = "https://beta.rmgppapp.com/api/";
+    public static final String CHECK_SUPERVISOR_URL = BASE_URL + "checkSupervisor";
+    public static final String POST_ASSIGNED_WORKER_URL = BASE_URL  + "insertAssignedWorker"; //Unexpected response code 500
+    public static final String GET_HR_DATA_URL = BASE_URL + "hrData";
+    public static final String GET_PLANNING_DATA_URL = BASE_URL + "planningData";
+    public static final String GET_OPERATION_DATA_URL = BASE_URL + "operationData";
+    public static final String POST_HOURLY_DATA_URL = BASE_URL + "insertHourlyRecord"; //Unexpected response code 500
+    public static final String POST_LINE_DATA_URL = BASE_URL + "insertLineData"; //Unexpected response code 500
+    public static final String POST_LINE_TARGET_URL = BASE_URL + "insertLineTarget";
+    public static final String GET_HOURLY_RECORD_DATA = BASE_URL + "getHourlyData";
+    public static final String GET_ASSIGNED_WORKER_URL = BASE_URL + "getAssignedWorkerData";
+    public static final String GET_SUMMERY_DATA = BASE_URL + "getSummeryData";
+    public static final String POST_NEW_STYLE_URL = BASE_URL + "insertStyle";
+    public static final String CHECK_LINE_TARGET_URL = BASE_URL + "checkLineTarget";
+    public static final String GET_PROBLEM_DATA_URL = BASE_URL + "getProblems/Input";
+    public static final String GET_ALL_STYLES = BASE_URL + "getAllStyles"; // SELECT DISTINCT StyleNo FROM order_list
+    public static final String GET_ALL_STYLES_OB = BASE_URL + "getAllStylesOB";
+    public static final String GET_STYLE_DETAILS = BASE_URL + "getStyleDetails";
+    public static final String GET_STYLE_DETAILS_FROM_ORDERLIST = BASE_URL + "getStyleDetailsFromOrderList";
+    public static final String DELETE_STYLE_URL = BASE_URL + "deleteStyle";
+    public static final String DELETE_LINE_DATA_URL = BASE_URL + "deleteLineData";
+    public static final String GET_LINE_RECORD = BASE_URL + "getLineData";
+    public static final String UPDATE_LINE_DATA_STATUS = BASE_URL + "updateStatus";
+    public static final String UPDATE_STYLE_STATUS = BASE_URL + "updateStatusStyle";
+    public static final String GET_STYLE_SUMMERY = BASE_URL + "getLineSummary/";
+    public static final String GET_BUILDING_DATA = BASE_URL + "getBuildingData/";
+    public static final String CHECK_DEVICEID = BASE_URL + "checkValidFactory/";
 }
